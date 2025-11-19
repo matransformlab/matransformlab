2b6c4218ebbe58ca8ec52f2d0d103b9213a5fa63
matransformlab <matransformlab@gmail.com>
2025-11-19 18:49:24 +0300

    Add OrderConfirmationEmail component template

---
+++ emails/templates/order-confirmation.tsx
@@ -0,0 +1,9 @@
+export function OrderConfirmationEmail() {
+  return (
+    <div>
+      <h1>MA Transform Lab</h1>
+      <p>Your order has been confirmed!</p>
+      <p>Sovereignty. Systems. Scale.</p>
+    </div>
+  )
+}
